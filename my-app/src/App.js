72e931407e3975a1d67427df27d6e5cf9d0d54e9
jhnoh4268 <126999597+jhnoh4268@users.noreply.github.com>
2026-08-16 23:01:11 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd awas7
+          Learn GithubAction cicd awas8
         </a>
       </header>
     </div>
